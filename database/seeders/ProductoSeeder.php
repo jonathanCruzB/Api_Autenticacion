41f9e6f33b15e946207480a8ec4a3a68b3182207
jonathanCruzB <jonathan.cruz@uniminuto.edu.co>
2024-06-12 23:38:40 -0500

    Insertando datos a traves de los seeders

--- database/seeders/ProductoSeeder.php
+++ database/seeders/ProductoSeeder.php
@@ -4,14 +4,32 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProductoSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+    
+
     public function run(): void
     {
         //
+
+        DB::table('productos')->insert([
+            'name'=> 'Xiaomi 14 pro',
+            'descripcion'=> 'Telefono gama premium con excelente camara leica',
+            'precio'=> 4595000
+        ]);
+        DB::table('productos')->insert([
+            'name'=> 'Samsung S24+',
+            'descripcion'=> 'Telefono gama premium con IA',
+            'precio'=> 5595000
+
+        ]);
+        DB::table('productos')->insert([
+            'name'=> 'Pixel 8 pro',
+            'descripcion'=> 'Telefono gama premium con androi puro',
+            'precio'=> 3595000
+
+        ]);
     }
 }
